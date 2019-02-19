8ac83cc5e1297c770645e63e44be652b7f890ef0
unknown <ramsaec@gmail.com>
2019-02-19 12:35:01 +0530

    Get todo by id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const {mongoose} = require('./db/db');
 const {Todo} = require('./models/Todo');
-const User = require('./models/User');
+const {User} = require('./models/User');
+const {ObjectID} = require('mongodb')
 let express = require('express');
 let bodyParser = require('body-parser');
 
@@ -16,6 +17,45 @@ app.post('/todos', (req,res) => {
     }, (error) => {
         res.status(400).send(error);
     })
+});
+
+app.get('/todos', (req,res) => {
+    Todo.find().then(todos => {
+        res.send({todos});
+    }).catch(e => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req,res) => {
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send('Invalid ID');
+        return;
+    }
+    Todo.findById(id)
+        .then((todo) => {
+            if(!todo){
+                res.status(400).send('Todo not found');
+                return;
+            }
+            res.send({todo});
+        }).catch(e => res.status(400).send(e));
+});
+
+app.post('/users', (req,res) => {
+    let user = new User({
+        text: req.body.text
+    });
+    user.save().then(result => {
+        res.send(result);
+    }, (error) => {
+        res.status(400).send(error);
+    })
+});
+
+app.get('/users', (req,res) => {
+    User.find().then(users => {
+        res.send({users});
+    }).catch(e => res.status(400).send(e));
 });
 
 app.listen(3000, () => {
