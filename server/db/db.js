3fdb11b4779ab67af332a3f79067cc4da42b453a
unknown <ramsaec@gmail.com>
2019-03-24 19:32:53 +0530

    Development and test envs

--- server/db/db.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser : true});
-
-module.exports = {
-    mongoose
-};
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-const {mongoose} = require('./db/db');
+require('./config/config');
+const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/Todo');
 const {User} = require('./models/User');
 const {ObjectID} = require('mongodb')
@@ -9,7 +10,7 @@ const _ = require('lodash');
 let app = express();
 app.use(bodyParser.json());
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port;
 
 app.post('/todos', (req,res) => {
     let todo = new Todo({
