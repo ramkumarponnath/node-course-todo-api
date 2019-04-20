2fde9722a14927270b64d37daeebc530c6887387
unknown <ramsaec@gmail.com>
2019-04-20 20:26:35 +0530

    Add Post /users route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,11 @@
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser : true});
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser : true, 
+    useCreateIndex: true,
+    keepAlive: 120
+});
 
 module.exports = {
     mongoose
--- server/models/user.js
+++ server/models/user.js
@@ -65,6 +65,25 @@ UserSchema.statics.findByToken = function(token) {
     });
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    let User = this;
+    return User.findOne({email}).then(user => {
+        if(!user) {
+            return Promise.reject('User not found');
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user);
+                } else {
+                    reject('Password mismatch');
+                }
+            })
+        })
+        
+    });
+}
+
 UserSchema.pre('save', function (next) {
     const user = this;
     if(user.isModified('password')) {
